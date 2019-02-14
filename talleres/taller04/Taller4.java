c6cd28fda1e8ca1131e671a989d55f78e586f2e6
daoterog <46933083+daoterog@users.noreply.github.com>
2019-02-13 20:42:30 -0500

    Update Taller4.java

--- talleres/taller04/Taller4.java
+++ talleres/taller04/Taller4.java
@@ -11,25 +11,40 @@ public class Taller4
     public static int arrayAux (int [] array, int n) {
         return n==array.length-1 ? array[n] : array[n]+arrayAux(array,n+1);
     }
+    
+    public static int arrayAux (int [] array, int n) { 
+        if (n==array.length-1) { //C1
+            return array[n]; //C2
+        } else {
+            return array[n]+arrayAux(array,n+1); //C3+T(n-1)
+        }
+    }
 
+    public static long fibonacci (int i) {
+        if (i<=1) {//C1
+            return i;//C2
+        } else 
+            return fibonacci(i-1)+fibonacci(i-2);//C3+T(n-1)
+        }
+    }
+    
     public static long fibonacci (int i) {
         return i <= 1 ? i : fibonacci(i-1)+fibonacci(i-2);
     }
 
     public static boolean groupSum (int start, int [] array, int target) {
-        if (start>=array.length) {
-            if (target==0) return true;
-            else return false;
-        } else if (array[start]>target) {
-            return groupSum(start+1,array,target);
-        } else if (target-array[start]==0) {
-            return true;
-        } else {
-            if (start==0) {
-                return groupSum(start+1,array,target);
+        if (start>=array.length) { //C1
+            if (target==0) {//C2 
+                return true;//C3
             } else {
-                return groupSum(start+1,array,target-array[start]) || groupSum(start+1,array,target);
+                return false;//C4
             }
+        } else if (array[start]>target) {//C5
+            return groupSum(start+1,array,target);//C6+T(n-1)
+        } else if (target-array[start]==0) {//C7
+            return true;//C8
+        } else {
+                return groupSum(start+1,array,target-array[start]) || groupSum(start+1,array,target);//C10+T(n-1)
         }
     }
 
