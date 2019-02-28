67bff45fff9e1511e5c77ee2b1941ad0e5753288
daoterog <46933083+daoterog@users.noreply.github.com>
2019-02-27 21:45:05 -0500

    Update tester.java

--- talleres/taller06/tester.java
+++ talleres/taller06/tester.java
@@ -1,3 +1,7 @@
+/**
+ * @rmateusc @daoterog
+ */
+
 public class tester
 {
     public void main (){
@@ -16,25 +20,23 @@ public class tester
         arraylist.add('e');
         arraylist.add('l');
         arraylist.add('m');
-        arraylist.add('a');
-        arraylist.add('t');
         arraylist.add('i');
+        arraylist.add('t');
+        arraylist.add('e');
         arraylist.add('u');
         arraylist.add('s');
         imprimir(arraylist);
         System.out.println(arraylist.size());
-        arraylist.add(9,'e');
+        arraylist.add(7,'a');
         imprimir(arraylist);
-        System.out.println(arraylist.size());
-        //imprimir(arraylist);
-        //arraylist.remove(10);
-        //imprimir(arraylist);
+        arraylist.remove(8);
+        imprimir(arraylist);
     }
     
     public void imprimir(textEditor arraylist){
         for (int i=0; i<arraylist.size();i++){
-            System.out.print(arraylist.get(i)+"");
+            System.out.print(arraylist.get(i));
+            if (i==arraylist.size()-1) System.out.println();
         }
     }
 }
-
