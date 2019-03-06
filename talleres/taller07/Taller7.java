b05d015e08252f8e8f0efff39de37897cbab7813
daoterog <46933083+daoterog@users.noreply.github.com>
2019-03-06 08:05:36 -0500

    Update Taller7.java

--- talleres/taller07/Taller7.java
+++ talleres/taller07/Taller7.java
@@ -1,138 +1,153 @@
+
 /**
  * @daoterog @rmateusc
  */
-import java.lang.ArrayIndexOutOfBoundsException;
-public class textEditor
-{    
-    char [] text= new char [0];
-
-    public textEditor(){
+public class LinkedListDaniel
+{
+    private Node first;
+    private int size;
+    private Node last;
+    public LinkedListDaniel()
+    {
+        size = 0;
+        first = null;	
     }
 
-    public void remove(int index) throws ArrayIndexOutOfBoundsException {
-        if (index<=text.length){
-            text[index]='\u0000';
-            reducirTamano(index,text, text.length-1);
-        } else 
-            throw new  ArrayIndexOutOfBoundsException();
+    private Node getNode(int index) throws IndexOutOfBoundsException {
+        if (index >= 0 && index < size) {
+            Node temp = first;
+            for (int i = 0; i < index; i++) {
+                temp = temp.next;
+            }
+            return temp;
+        } else {
+            throw new IndexOutOfBoundsException();
+        }
     }
 
-    public int size(){
-        return text.length;
+    public int get(int index) throws IndexOutOfBoundsException {
+        Node temp = getNode(index);
+        return temp.data;
     }
 
-    public char get(int index){
-        if (index<text.length){
-            return text[index];
-        } else 
-            throw new ArrayIndexOutOfBoundsException();
+    public int size () {
+        return size;
     }
-
-    public void add(char t){
-        agrandarTamano(text,text.length+1);
-        text[text.length-1]=t;
+    
+    public int indexOf (int data) {
+        if (contains(data)) {
+            return indexOfAux(first,data,0);
+        } else {
+            return -1;
+        }
     }
-
-    public void agrandarTamano(char [] text1,int tamano) {
-        text=new char[tamano];
-        for (int i=0;i<text1.length;i++) {
-            text[i]=text1[i];  
-        }        
-        text[text.length-1]=0;
+    
+    public int indexOfAux(Node nodo, int data,int contador) {
+        return nodo.data==data ? contador : indexOfAux(nodo.next,data,contador++);
     }
-
-    public void reducirTamano (int index, char []text1, int tamano){
-        text = new char [tamano];
-        for (int i=0; i<tamano;i++){
-            if (i<index){
-                text[i] = text1[i];
-            } else{
-                text[i]=text1[i+1];
+    
+    public int max () {
+        return maxAux(first,first.data,0);
+    }
+    
+    public int maxAux (Node nodo, int max,int contador) {
+        if (contador==size) {
+            return max;
+        } else {
+            if (max<nodo.data) {
+                return maxAux(nodo.next,nodo.data,contador++);
+            } else {
+                return maxAux(nodo.next,max,contador++);
             }
         }
     }
-
-    public void add(int index, char t){
-        if (index<=text.length){
-            char ultima=text[text.length-1];
-            agrandarTamano(text,text.length+1);
-            organizador(index,t,ultima);
-        } else 
-            agrandarTamano(text,text.length+(index+1-text.length));
-        text[index]=t;
-    }
-
-    public void organizador (int index, char t, char ultima){
-        char remplazo=text[index];
-        char remp;
-        for (int i=index; i<text.length-1;i++){
-            if (i==index) {
-                text[i]=t;
-            } else{
-                remp = text[i];
-                text[i]=remplazo;
-                remplazo=remp;
-            }
-        }
-        text[text.length-1]=ultima;
-    }
     
-    public boolean contains (char a) {
-        for (int i=0;i<text.length;i++) {
-            if (a==text[i]) 
-            return true;
-        }
-        return false;
-    }
-    
-    public int indexOf (char a) {
-        for (int i=0;i<text.length;i++) {
-            if (a==text[i]) 
-            return i;
-        }
-        return -1;
-    }
-
-    public void imprimir(char [] arraylist){
-        for (int i=0; i<arraylist.length;i++){
-            if (i!=arraylist.length) 
-                System.out.print(arraylist[i]);
-            else 
-                System.out.print(arraylist[i]+" ");
-        }
-    }
-    
-    public boolean compararClases (char[] array1, char[] array2){
-        return auxComparar(array1,array2,0);
-    }
-    
-    public boolean auxComparar(char[]array1, char[]array2, int index){
-        if (array1[index]==array2[index]){
-            if (index==array1.length && index==array2.length){
-                return true;
-            }
-            return auxComparar(array1,array2,index+1);
+    public boolean equals (LinkedListDaniel l1, LinkedListDaniel l2) {
+        if (l1.size()==l2.size()) {
+            return equalsAux(l1.first,l2.first,0,l1.size);
         } else {
             return false;
         }
     }
     
-    public int convertInteger(char []array1){
-        int []array2 = new int [array1.length];
-        for (int i=0; i<array1.length;i++){
-            int pos=Character.getNumericValue(i);
-            array2[i]=pos;
+    public boolean equalsAux (Node l1, Node l2, int contador, int size) {
+        return l1.data==l2.data ? contador==size ? true : equalsAux(l1.next,l2.next,contador++,size) : false;
+    }
+
+    public void insert (int data, int index) throws IndexOutOfBoundsException {
+        if (index>1) {
+            if(index==0) {
+                size++;
+                Node agregar=new Node(data);
+                agregar.next=first;
+                first=agregar;
+            } else if (index<size) {
+                size++;
+                insertAux1(first,index,0,data);
+            } else {
+                int dif=index-size+1;
+                size+=dif;
+                insertAux2(dif,data);
+            }
+        } else {
+            throw new IndexOutOfBoundsException();
         }
-        return max(array2,0);
     }
     
-    public int max(int [] array, int index){
-        int max = array[0];
-        if (index==array.length){
-            return max;
-        } else {
-            max= Math.max(array[index+1],max(array,index+1));
-            return max;
+    public void getLast () {
+        Node ultimo=first;
+        for (int i=0;i<size-1;i++) {
+            ultimo=ultimo.next;
         }
+        last=ultimo;
+    }
+
+    public void insertAux2 (int dif,int data) {
+        if (dif>1) {
+            Node agregar=new Node(0);
+            getLast();
+            last.next=agregar;
+            insertAux2(dif-1,data);
+        } else {
+            Node agregar=new Node(data);
+            getLast();
+            last.next=agregar;
+        }
+    }
+
+    public void insertAux1 (Node nodo,int index, int contador, int data) {
+        if (contador<index-1) {
+            insertAux1(nodo,index,contador++,data);
+        } else {
+            Node agregar=new Node(data);
+            Node cambiaso=nodo.next;
+            nodo.next=agregar;
+            agregar.next=cambiaso;
+        }
+    }
+
+    public void remove (int index) throws IndexOutOfBoundsException {
+        if (index>=0 && index<size) {
+            if (index==0) first=first.next;
+            else {
+                Node cambiar=removeAux(first,0,index-1);
+                cambiar.next=cambiar.next.next;
+            }
+            size--;
+        } else {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    public Node removeAux (Node nodo, int contador, int index) {
+        return contador==index ? nodo : removeAux(nodo.next,contador++,index);
+    }
+
+    public boolean contains (int data) {
+        return containsAux(first,data,0);
+    }
+
+    public boolean containsAux (Node nodo, int data, int index) {
+        return index==size ? false : nodo.data==data ? true : containsAux(nodo.next,data,index++);
     }
 }
