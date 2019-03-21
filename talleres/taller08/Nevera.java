6477ba1f934704cc23abb17e68ab364dfc92329a
rmateusc <46933028+rmateusc@users.noreply.github.com>
2019-03-20 22:28:23 -0500

    Create Nevera.java

---
+++ talleres/taller08/Nevera.java
@@ -0,0 +1,17 @@
+public class Nevera
+{
+    int numero;
+    String descripcion;
+    public Nevera (int numero, String descripcion){
+        this.numero=numero;
+        this.descripcion=descripcion;
+    }
+    
+    public int getNumero(){
+        return numero;
+    }
+    
+    public String getDescripcion(){
+        return descripcion;
+    }
+}
