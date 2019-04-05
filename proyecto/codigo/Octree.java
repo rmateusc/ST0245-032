ed454031cd91839db77c43424ea50d9fb99a5eaf
daoterog <46933083+daoterog@users.noreply.github.com>
2019-04-05 02:48:43 -0500

    Update Octree.java

--- proyecto/codigo/Octree.java
+++ proyecto/codigo/Octree.java
@@ -6,7 +6,7 @@ import java.util.LinkedList;
 public class Octree
 {
     private ArrayList<ArrayList> tabla=new ArrayList(8);
-    private double midD,midW,midH,diagonal;
+    private double midD,midW,midH;
     public Octree(ArrayList<Bee> abejas,ArrayList<Double> mins,double midD,double midW,double midH) {
         for (int i=0;i<8;++i) {
             ArrayList<Bee> ab=new ArrayList();
@@ -15,17 +15,10 @@ public class Octree
         this.midD=midD;
         this.midW=midW;
         this.midH=midH;
-        double ph=Math.sqrt(Math.pow(midD*111325,2)+Math.pow(midW*111325,2));
-        diagonal=Math.sqrt(Math.pow(ph,2)+Math.pow(midH,2));
-        if (diagonal>100) {
-            for (int i=0;i<abejas.size();++i) {
-                hashing(abejas.get(i),mins);
-            }
-            nuevoOct(mins);
-        } else {
-            if (abejas.size()>1) choque(abejas);
-            else System.out.println("ABEJA SOLAA");
+        for (int i=0;i<abejas.size();++i) {
+            hashing(abejas.get(i),mins);
         }
+        nuevoOct(mins);
     }
 
     public void choque(ArrayList<Bee> abejas) {
@@ -68,26 +61,34 @@ public class Octree
     }
 
     public void nuevoOct(ArrayList<Double> mins) {
-        for (int i=0;i<8;++i) {
-            if (tabla.get(i).size()!=0) {
-                if (i==0) {
-                    Octree oct=new Octree(tabla.get(i),mins,midD/2,midW/2,midH/2);
-                } else if (i==1) {
-                    Octree oct=new Octree(tabla.get(i),mins,midD/2,midW/2,3*midH/2);
-                } else if (i==2) {
-                    Octree oct=new Octree(tabla.get(i),mins,midD/2,3*midW/2,midH/2);
-                } else if (i==3) {
-                    Octree oct=new Octree(tabla.get(i),mins,midD/2,3*midW/2,3*midH/2);
-                } else if (i==4) {
-                    Octree oct=new Octree(tabla.get(i),mins,3*midD/2,midW/2,midH/2);
-                } else if (i==5) {
-                    Octree oct=new Octree(tabla.get(i),mins,3*midD/2,midW/2,3*midH/2);
-                } else if (i==6) {
-                    Octree oct=new Octree(tabla.get(i),mins,3*midD/2,3*midW/2,midH/2);
-                } else {
-                    Octree oct=new Octree(tabla.get(i),mins,3*midD/2,3*midW/2,3*midH/2);
-                }
-            } 
+        double ph=Math.sqrt(Math.pow((midD-midD/2)*111325,2)+Math.pow((midW-midW/2)*111325,2));
+        double diagonal=Math.sqrt(Math.pow(ph,2)+Math.pow((midH-midH/2),2));
+        if (diagonal>100) {
+            for (int i=0;i<8;++i) {
+                if (tabla.get(i).size()!=0) {
+                    if (i==0) {
+                        Octree oct=new Octree(tabla.get(i),mins,midD/2,midW/2,midH/2);
+                    } else if (i==1) {
+                        Octree oct=new Octree(tabla.get(i),mins,midD/2,midW/2,3*midH/2);
+                    } else if (i==2) {
+                        Octree oct=new Octree(tabla.get(i),mins,midD/2,3*midW/2,midH/2);
+                    } else if (i==3) {
+                        Octree oct=new Octree(tabla.get(i),mins,midD/2,3*midW/2,3*midH/2);
+                    } else if (i==4) {
+                        Octree oct=new Octree(tabla.get(i),mins,3*midD/2,midW/2,midH/2);
+                    } else if (i==5) {
+                        Octree oct=new Octree(tabla.get(i),mins,3*midD/2,midW/2,3*midH/2);
+                    } else if (i==6) {
+                        Octree oct=new Octree(tabla.get(i),mins,3*midD/2,3*midW/2,midH/2);
+                    } else {
+                        Octree oct=new Octree(tabla.get(i),mins,3*midD/2,3*midW/2,3*midH/2);
+                    }
+                } 
+            }
+        } else {
+            for (int i=0;i<8;++i) {
+                choque(tabla.get(i));
+            }
         }
     }
 }
