2daf1cbd838df6017cdc28c4e59b1d19759799ba
rmateusc <46933028+rmateusc@users.noreply.github.com>
2019-04-16 16:59:41 -0500

    Add files via upload

---
+++ laboratorios/lab04/codigo/Directorio.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+/** 
+ * @rmateusc @daoterog
+ */
+public class Directorio
+{    
+    String dir;
+    ArrayList<File>info=new ArrayList<File>();
+    public Directorio (String dir){
+        this.dir=dir;
+    }
+}
+ 
