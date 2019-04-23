bee8f7e4e5ad8aa2b7d04a7d57a1b93e75972ee6
daoterog <46933083+daoterog@users.noreply.github.com>
2019-04-23 14:23:53 -0500

    Update Octree.java

--- proyecto/codigo/Octree.java
+++ proyecto/codigo/Octree.java
@@ -13,15 +13,17 @@
  * @see Bee
  */
 import java.util.ArrayList;
+import java.util.LinkedList;
 public class Octree
 {
-    private ArrayList<ArrayList> tabla=new ArrayList(8);
     private double midD,midW,midH;
     /**
-     * Este es el constructor de nuestra estructura de datos, este se encargara de llenar el ArrayList tabla, con sus respetivos ArrayList
-     * de Bee, asi como de relacionar cada los parametros midD, midW y midH con su respectivo atributo. Desde este metodo se llamara el metodo
-     * hashing para cada abeja, para asi poder ubicar a cada abeja en su respectivo sector. Despues llamara el metodo nuevoOct, poder 
-     * determinar si se puede realizar otro Octree o no.
+     * Este sera el metodo que representara el Octree, sobre este metodo se realizara toda la recursion, aqui se asignara cada abeja a 
+     * su respectivo sector del Octree, representado por un ArrayList de 8 espacios (cada uno representando cada sector), donde sera 
+     * guardado en un LinkedList. El proceso de decidir en que sector va cada abeja lo hara el metodo hashing. Despues de que se ubique
+     * cada abeja en un sector se pasara a crear las subidvisiones del Octree con el metodo nuevoOctree. Antes de pasar a crear las nuevas
+     * subdivisiones del Octree se pregunta si la diagonal octual es menor a los 100 metros, si asi lo es pasara a imprimir las abejas 
+     * ubicadas en los sectores donde hay mas de una abeja.
      * @param ArrayList<Bee> abejas: ArrayList con todas la abejas
      * @param ArrayList<Double> mins: son los valores minimos de coordenadas, a los que se le sumaran otros parametros para determinar 
      * la posicion de la abeja. Tomelo como si fuera uno de los vertices del octree.
@@ -31,148 +33,154 @@ public class Octree
      * @see hashing
      * @see nuevoOct
      */
-    public Octree(ArrayList<Bee> abejas,ArrayList<Double> mins,double midD,double midW,double midH) {
-        for (int i=0;i<8;++i) {
-            ArrayList<Bee> ab=new ArrayList();
-            tabla.add(ab);
-        }
+    public void octree (LinkedList<Bee> abejas,ArrayList<Double> mins,double midD,double midW,double midH) {
         this.midD=midD;
         this.midW=midW;
         this.midH=midH;
-        for (int i=0;i<abejas.size();++i) {
-            hashing(abejas.get(i),mins);
+        ArrayList<LinkedList<Bee>> octree=new ArrayList(8); //Octree
+        for (int i=0;i<8;++i) {
+            LinkedList<Bee> ab=new LinkedList();
+            octree.add(ab);
         }
-        nuevoOct(mins);
-    }
-
-    /**
-     * Este metodo se encarga de imprimir las coordenadas de todas la abejas 
-     * del conjunto de datos, ya que si se llego a llamar a este metodo es 
-     * porque todas estan en peligro de chocarse.
-     */
-    public void choque(ArrayList<Bee> abejas) {
-        System.out.println("Las abejas en las siguientes coordenadas estan en peligro de chocarse");
         for (int i=0;i<abejas.size();++i) {
-            System.out.println(abejas.get(i).getLatitude()+","+abejas.get(i).getLongitude()+","+abejas.get(i).getAltitude());
+            Bee abeja=abejas.poll();
+            int sector=hashing(abeja,mins);
+            octree.get(sector).add(abeja);
+        }
+        double ph=Math.sqrt(Math.pow((midD)*111325,2)+Math.pow((midW)*111325,2)); //Primera hipotenusa
+        double diagonal=Math.sqrt(Math.pow(ph,2)+Math.pow((midH),2)); 
+        if (diagonal>100) {
+            for (int i=0;i<8;++i) {
+                if (octree.get(i).size()>1) {
+                    nuevoOctree(octree.get(i),mins,i);
+                }
+            }
+        } else {
+            for (int i=0;i<8;++i) {
+                if (octree.get(i).size()>0) choque(octree.get(i));
+            }
         }
     }
 
     /**
      * Este metodo se encargara de recibir cada abeja que se encuentra en el octree, y ubicarla en un determinado sector de la estructura 
-     * de datos, de manera que lo ubicara en el espacio del ArrayList tabla correspondiente al sector en el que esta posicionada la abeja
+     * de datos, de manera que lo ubicara en el espacio del ArrayList tabla correspondiente al sector en el que esta posicionada la abeja. 
+     * Determinara el sector comparando los minimos, es decir, los vertices del Octree, mas los valores medios del Octree con el que se esta
+     * trabajando.
      * @param Bee abeja: abeja que sera ubicada
      * @param ArrayList<Double> mins: este ArrayList contendra los minimos, es decir, el vertice compuesto por cada coordenada. Este parametro
      * es necesario para poder hacer una ubicacion adecuada de cada abeja, debido a que la utilizacion de este parametro nos simplificara 
      * el codigo de manera que solo se le tendra que ser sumado un valor para determinar si una abeja se encuentra en un sector o en otro.
+     * @return numero del sector en el que estara ubicado.
      */
-    private void hashing(Bee abeja,ArrayList<Double> mins) {
+    private int hashing(Bee abeja,ArrayList<Double> mins) {
         if (abeja.getLatitude()<=mins.get(0)+midD) {
             if (abeja.getLongitude()<=mins.get(1)+midW) {
                 if (abeja.getAltitude()<=mins.get(2)+midH) {
-                    tabla.get(0).add(abeja);
+                    return 0;
                 } else {
-                    tabla.get(1).add(abeja);
+                    return 1;
                 }
             } else {
                 if (abeja.getAltitude()<=mins.get(2)+midH) {
-                    tabla.get(2).add(abeja);
+                    return 2;
                 } else {
-                    tabla.get(3).add(abeja);
+                    return 3;
                 }
             }
         } else {
             if (abeja.getLongitude()<=mins.get(1)+midW) {
                 if (abeja.getAltitude()<=mins.get(2)+midH) {
-                    tabla.get(4).add(abeja);
+                    return 4;
                 } else {
-                    tabla.get(5).add(abeja);
+                    return 5;
                 }
             } else {
                 if (abeja.getAltitude()<=mins.get(2)+midH) {
-                    tabla.get(6).add(abeja);
+                    return 6;
                 } else {
-                    tabla.get(7).add(abeja);
+                    return 7;
                 }
             }
         }
     }
 
     /**
-     * Este metodo se encargara de decidir si la diagonal de los sectores del siguiente Octree seran menores o mayores a 100metros de longitud.
-     * Esto se hace ya que si la diagonal es menor a 100 metros esto significa que todas las abejas ubicadas en ese sector estan en peligro de
-     * chocarse la una con la otra. Si la diagonal no es menor a 100 metros procede a crear un nuevo Octree con todas las abejas ubicadas
-     * en cada sector. Antes de hacer eso, identifica que sector es donde sera creado un nuevo Octree para asi poder redefinir los vertices 
-     * de cada nueva estructura. 
+     * Este metodo se encarga de hacer la recursion, con respecto al metodo principal octree. Recibe los parametro correspondientes a las 
+     * abejas ubicada en el sector, y los vertices de este, que se encuentran en el ArrayLis mins. Con el paramtero sector identifican 
+     * como deben ser redefinidos los vertices del siguiente suboctree, ya que estos varian por sector.
+     * @param LinkedList<Bee> abejas: abejas del sector correspondiente
      * @param ArrayList<Double> mins: son los valores minimos de coordenadas, a los que se le sumaran otros parametros para redefinir los
      * vertices del siguiente Octree. Tomelo como si fuera uno de los vertices del octree.
+     * @param int sector: sector en el cual esta ubicado
      */
-    public void nuevoOct(ArrayList<Double> mins) {
-        double ph=Math.sqrt(Math.pow((midD/2)*111325,2)+Math.pow((midW/2)*111325,2));
-        double diagonal=Math.sqrt(Math.pow(ph,2)+Math.pow((midH/2),2));
-        if (diagonal>100) {
-            for (int i=0;i<8;++i) {
-                if (tabla.get(i).size()>0) {
-                    if (i==0) {
-                        Octree oct=new Octree(tabla.get(i),mins,midD/2,midW/2,midH/2);
-                    } else if (i==1) {
-                        double nuevoH=mins.get(2)+midH;
-                        mins.remove(2);
-                        mins.add(2,nuevoH);
-                        Octree oct=new Octree(tabla.get(i),mins,midD/2,midW/2,midH/2);
-                    } else if (i==2) {
-                        double nuevoW=mins.get(1)+midW;
-                        mins.remove(1);
-                        mins.add(1,nuevoW);
-                        Octree oct=new Octree(tabla.get(i),mins,midD/2,midW/2,midH/2);
-                    } else if (i==3) {
-                        double nuevoH=mins.get(2)+midH;
-                        mins.remove(2);
-                        mins.add(2,nuevoH);
-                        double nuevoW=mins.get(1)+midW;
-                        mins.remove(1);
-                        mins.add(1,nuevoW);
-                        Octree oct=new Octree(tabla.get(i),mins,midD/2,midW/2,midH/2);
-                    } else if (i==4) {
-                        double nuevoD=mins.get(0)+midD;
-                        mins.remove(0);
-                        mins.add(0,nuevoD);
-                        Octree oct=new Octree(tabla.get(i),mins,midD/2,midW/2,midH/2);
-                    } else if (i==5) {
-                        double nuevoD=mins.get(0)+midD;
-                        mins.remove(0);
-                        mins.add(0,nuevoD);
-                        double nuevoH=mins.get(2)+midH;
-                        mins.remove(2);
-                        mins.add(2,nuevoH);
-                        Octree oct=new Octree(tabla.get(i),mins,midD/2,midW/2,midH/2);
-                    } else if (i==6) {
-                        double nuevoD=mins.get(0)+midD;
-                        mins.remove(0);
-                        mins.add(0,nuevoD);
-                        double nuevoW=mins.get(1)+midW;
-                        mins.remove(1);
-                        mins.add(1,nuevoW);
-                        Octree oct=new Octree(tabla.get(i),mins,midD/2,midW/2,midH/2);
-                    } else {
-                        double nuevoD=mins.get(0)+midD;
-                        mins.remove(0);
-                        mins.add(0,nuevoD);
-                        double nuevoW=mins.get(1)+midW;
-                        mins.remove(1);
-                        mins.add(1,nuevoW);
-                        double nuevoH=mins.get(2)+midH;
-                        mins.remove(2);
-                        mins.add(2,nuevoH);
-                        Octree oct=new Octree(tabla.get(i),mins,midD/2,midW/2,midH/2);
-                    }
-                } 
-            }
+    public void nuevoOctree(LinkedList<Bee> abejas,ArrayList<Double> mins,int sector) {
+        if (sector==0) {
+            octree(abejas,mins,midD/2,midW/2,midH/2);
+        } else if (sector==1) {
+            double nuevoH=mins.get(2)+midH;
+            mins.remove(2);
+            mins.add(2,nuevoH);
+            octree(abejas,mins,midD/2,midW/2,midH/2);
+        } else if (sector==2) {
+            double nuevoW=mins.get(1)+midW;
+            mins.remove(1);
+            mins.add(1,nuevoW);
+            octree(abejas,mins,midD/2,midW/2,midH/2);
+        } else if (sector==3) {
+            double nuevoH=mins.get(2)+midH;
+            mins.remove(2);
+            mins.add(2,nuevoH);
+            double nuevoW=mins.get(1)+midW;
+            mins.remove(1);
+            mins.add(1,nuevoW);
+            octree(abejas,mins,midD/2,midW/2,midH/2);
+        } else if (sector==4) {
+            double nuevoD=mins.get(0)+midD;
+            mins.remove(0);
+            mins.add(0,nuevoD);
+            octree(abejas,mins,midD/2,midW/2,midH/2);
+        } else if (sector==5) {
+            double nuevoD=mins.get(0)+midD;
+            mins.remove(0);
+            mins.add(0,nuevoD);
+            double nuevoH=mins.get(2)+midH;
+            mins.remove(2);
+            mins.add(2,nuevoH);
+            octree(abejas,mins,midD/2,midW/2,midH/2);
+        } else if (sector==6) {
+            double nuevoD=mins.get(0)+midD;
+            mins.remove(0);
+            mins.add(0,nuevoD);
+            double nuevoW=mins.get(1)+midW;
+            mins.remove(1);
+            mins.add(1,nuevoW);
+            octree(abejas,mins,midD/2,midW/2,midH/2);
         } else {
-            for (int i=0;i<8;++i) {
-                for (int i=0;i<8;++i) {
-                if (tabla.get(i).size()>0) choque(tabla.get(i));
-            }
-            }
+            double nuevoD=mins.get(0)+midD;
+            mins.remove(0);
+            mins.add(0,nuevoD);
+            double nuevoW=mins.get(1)+midW;
+            mins.remove(1);
+            mins.add(1,nuevoW);
+            double nuevoH=mins.get(2)+midH;
+            mins.remove(2);
+            mins.add(2,nuevoH);
+            octree(abejas,mins,midD/2,midW/2,midH/2);
         }
     }
+
+    /**
+     * Este metodo se encarga de imprimir las coordenadas de todas la abejas 
+     * del sector, ya que si se llego a llamar a este metodo es 
+     * porque todas estan en peligro de chocarse.
+     * @param LinkedList<Bee> abejas: abejas del sector
+     */
+    public void choque(LinkedList<Bee> abejas) {
+        //System.out.println("Las abejas en las siguientes coordenadas estan en peligro de chocarse");
+        for (Bee abeja:abejas) {
+            System.out.println(abejas.poll().getLatitude()+","+abejas.poll().getLongitude()+","+abejas.poll().getAltitude());
+        }
+    }
+
 }
