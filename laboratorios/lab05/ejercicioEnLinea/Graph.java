557acd30308ff1f88d57fb03966100c4aeaa2e83
rmateusc <46933028+rmateusc@users.noreply.github.com>
2019-05-05 15:07:19 -0500

    Create Graph.java

---
+++ laboratorios/lab05/ejercicioEnLinea/Graph.java
@@ -0,0 +1,52 @@
+import java.util.LinkedList;
+import java.util.Arrays;
+import java.util.Iterator; 
+/**
+ * @rmateusc @daoterog
+ */
+class Graph{
+    private static int vertex; 
+    private static LinkedList<Integer>adyacent[];
+    Graph(int vertex){
+        this.vertex=vertex;
+        adyacent = new LinkedList[vertex];
+        
+        for (int i=0; i<vertex; i++) {
+            adyacent[i] = new LinkedList();
+        }
+    }
+    
+    void addEdge(int m, int n) {
+        adyacent[m].add(n);
+        adyacent[n].add(m);
+    }
+
+    static void coloring() {
+        int arr[] = new int[vertex];
+        Arrays.fill(arr, -1);
+        arr[0] = 0;
+        boolean b[] = new boolean[vertex];
+        Arrays.fill(b, true);
+        for (int i=1; i<vertex; i++) {
+            Iterator <Integer> it= adyacent[i].iterator();
+            while(it.hasNext()) {
+                int num = it.next();
+                if (arr[num] != -1) {
+                    b[arr[num]] = false;
+                }
+            }
+            int k;
+            for (k=0; k<vertex; k++) {
+                if (b[k]) {
+                    break;
+                }
+            }
+            arr[i]=k;
+            if (k>=2) {
+                System.out.println("NOT Bicoloreable");
+            }
+            Arrays.fill(b, true);
+        }
+        System.out.println("Bicoloreable");
+    }
+} 
